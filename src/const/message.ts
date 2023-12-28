d9fd2f643e98ddcd3a3f3b2c64cf3aa87b4b1563
xiangyu97Wang <107662515+xiangyu97Wang@users.noreply.github.com>
2023-12-28 01:11:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/const/message.ts
@@ -0,0 +1,6 @@
+export const LOADING_FLAT = '...';
+
+//  start with this，it should be a function message
+export const FUNCTION_MESSAGE_FLAG = '{"tool_calls"';
+
+export const FUNCTION_LOADING = 'FUNCTION_LOADING';
